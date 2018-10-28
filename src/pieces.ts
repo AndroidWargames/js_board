fa350113525c6567a346ca0bdcc953048e5dfcbe
Andrew Wagner <adwagner89@gmail.com>
2018-10-28 14:51:32 -0500

    Add tests for valid pawn movement

--- src/pieces.ts
+++ src/pieces.ts
@@ -1,63 +1,68 @@
-import Square from "../src/square"
+import Square from '../src/square'
 
-export default interface IPiece
-{
-  square?: Square;
-  white: boolean;
-  canAttack(s: Square): boolean;
-  canMoveTo(s: Square): boolean;
-  moveTo(s: Square): void;
+export default interface IPiece {
+  square?: Square
+  white: boolean
+  canAttack(s: Square): boolean
+  canMoveTo(s: Square): boolean
+  moveTo(s: Square): void
 }
 
-
-export default class Pawn
-{
-  square?: Square;
-  white: boolean;
-  hasMoved: boolean;
-  constructor(square: Square, white: boolean)
-  {
-    this.square = square;
-    this.square.piece = this;
-    this.white = white;
-    this.hasMoved = false;
+export default class Pawn {
+  square?: Square
+  white: boolean
+  hasMoved: boolean
+  constructor(square: Square, white: boolean) {
+    this.square = square
+    this.square.piece = this
+    this.white = white
+    this.hasMoved = false
   }
-  canMoveTo(s: Square)
-  {
-    if (s == undefined || this.square == undefined) { return false; }
-    if (!s.isEmpty() || !s.inbounds) { return false; }
-    if (s.column != this.square.column) { return false; }
-    var moveDistance = this.hasMoved ? 1 : 2;
-    if (moveDistance == 2)
-    {
-      var midSquare = this.square.board.getSquare(this.square.row + this.moveDirection(), this.square.column);
-      if (!this.canMoveTo(midSquare)) return false;
+  canMoveTo(s: Square) {
+    if (s == undefined || this.square == undefined) {
+      return false
     }
-    var moveAmount = this.moveDirection() * moveDistance;
-    var squareInRange = s.row > this.square.row && s.row - moveAmount <= this.square.row;
-    return squareInRange;
+    if (!s.isEmpty() || !s.inbounds) {
+      return false
+    }
+    if (s.column != this.square.column) {
+      return false
+    }
+    var allowedMoveDistance = this.hasMoved ? 1 : 2
+    var actualMoveDistance = (s.row - this.square.row) * this.moveDirection()
+    if (actualMoveDistance <= 0 || actualMoveDistance > allowedMoveDistance) return false
+    if (actualMoveDistance == 2) {
+      var midSquare = this.square.board.getSquare(
+        this.square.row + this.moveDirection(),
+        this.square.column
+      )
+      if (!this.canMoveTo(midSquare)) return false
+    }
+    return true
   }
 
-  moveDirection()
-  {
-    return this.white ? 1 : -1;
+  moveDirection() {
+    return this.white ? 1 : -1
   }
 
-  canAttack(s: Square)
-  {
-    if (s == undefined || this.square == undefined) { return false; }
-    if (!s.hasPieceWithColor(!this.white)) { return false; }
-    if (Math.abs(s.column - this.square.column) != 1) { return false; }
-    if (s.row - this.square.row != this.moveDirection()) return false;
-    return true;
+  canAttack(s: Square) {
+    if (s == undefined || this.square == undefined) {
+      return false
+    }
+    if (!s.hasPieceWithColor(!this.white)) {
+      return false
+    }
+    if (Math.abs(s.column - this.square.column) != 1) {
+      return false
+    }
+    if (s.row - this.square.row != this.moveDirection()) return false
+    return true
   }
 
-  moveTo(s: Square)
-  {
-    this.square!.piece = undefined;
-    this.square = s;
-    s.piece = this;
-  } 
+  moveTo(s: Square) {
+    this.square!.piece = undefined
+    this.square = s
+    s.piece = this
+    this.hasMoved = true
+  }
 }
-
-
--- test/pieces.test.ts
+++ test/pieces.test.ts
@@ -2,24 +2,50 @@ import Board from '../src/board'
 import Square from '../src/square'
 import Pawn from '../src/pieces'
 
-var shape = ['OOOOO', 'OOOOO', 'OOOOO', 'OOOOO', 'OOOOO']
-var board = new Board(shape)
+function newBoard() {
+  var shape = ['OOOOO', 'OOOOO', 'OOOOO', 'OOOOO', 'OOOOO']
+  var board = new Board(shape)
+  return board
+}
+
 describe('Pawn', () => {
   it('is instantiable', () => {
-    var s = new Square(true, 0, 0, board)
+    var dummyboard = newBoard()
+    var s = new Square(true, 0, 0, dummyboard)
     expect(new Pawn(s, true)).toBeInstanceOf(Pawn)
   })
 
   it('can call moveTo', () => {
+    var board = newBoard()
     var s = new Square(true, 0, 0, board)
     var s2 = new Square(true, 1, 1, board)
     var p = new Pawn(s, true)
+    expect(p.hasMoved).toBeFalsy()
     p.moveTo(s2)
     expect(s.isEmpty()).toBeTruthy()
     expect(p.square).toEqual(s2)
+    expect(p.hasMoved).toBeTruthy()
   })
 
-  it('evaluates white moves correctly', () => {})
+  it('evaluates valid white moves correctly', () => {
+    var board = newBoard()
+    var p = new Pawn(board.getSquare(1, 1), true)
+    var forwardOneSquare = board.getSquare(2, 1)
+    expect(p.canMoveTo(forwardOneSquare)).toBeTruthy()
+    var forwardTwoSquares = board.getSquare(3, 1)
+    expect(p.canMoveTo(forwardTwoSquares)).toBeTruthy()
+    p.moveTo(forwardOneSquare)
+    expect(p.canMoveTo(forwardTwoSquares)).toBeTruthy()
+  })
 
-  it('evaluates black moves correctly', () => {})
+  it('evaluates black moves correctly', () => {
+    var blackBoard = newBoard()
+    var p = new Pawn(blackBoard.getSquare(4, 1), false)
+    var forwardOneSquare = blackBoard.getSquare(3, 1)
+    expect(p.canMoveTo(forwardOneSquare)).toBeTruthy()
+    var forwardTwoSquares = blackBoard.getSquare(2, 1)
+    expect(p.canMoveTo(forwardTwoSquares)).toBeTruthy()
+    p.moveTo(forwardOneSquare)
+    expect(p.canMoveTo(forwardTwoSquares)).toBeTruthy()
+  })
 })
